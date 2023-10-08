cd08c1f2b111d339c4b7de45b46488209a1d7520
Rakesh <rakesh.h7997@gmail.com>
2023-10-08 11:58:13 +0530

    Add js

--- Book.js
+++ Book.js
@@ -8,3 +8,8 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+let newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click",function () {
+  alert("HELLO WORLD");
+})
